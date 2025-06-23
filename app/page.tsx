ad40a3d7e7efa02ea6ccc8c754dbfea9de6af0d1
wiigwiig10 <wiigwiig10@gmail.com>
2025-06-23 16:05:40 +0300

    meta

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,7 @@ import HeroCarouselPL from "./components/sections/HeroCarouselPL";
 import BestSellers from "./components/sections/BestSellers";
 import { ModalProvider } from '../app/context/ModalContext'
 import ProductModal from './components/ProductModal'
+import Head from "next/head";
 
 export default function Home() {
   const OPTIONS: EmblaOptionsType = { loop: true }
@@ -22,6 +23,34 @@ const SLIDE_COUNT = 8
 const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
 
   return (
+    <>
+          <Head>
+        {/* Meta Pixel Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1054693372933429');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1054693372933429&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </Head>
     <div className="flex min-w-full overflow-x-hidden w-full bg-transparent flex-col items-center pt-14 justify-center h-auto min-h-screen ">
       {/* <Hero/> */}
       {/* <PrismaCarousel/> */}
@@ -35,5 +64,6 @@ const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
       {/* <Mummies/> */}
 
     </div>
+    </>
   );
 }
