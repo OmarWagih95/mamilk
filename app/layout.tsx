7dcfbe273a2f7cf62d536abdcd21e69e7bde6c5d
wiigwiig10 <wiigwiig10@gmail.com>
2025-07-14 20:50:31 +0300

    meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,8 @@ import AnnouncmentBar from "./components/AnnouncmentBar";
 import { ModalProvider } from "./context/ModalContext";
 import ProductModal from "./components/ProductModal";
 import { CartProvider } from "./context/cartContext";
+import Script from "next/script";
+import Head from "next/head";
 
 export const testFont = localFont({
   src: "/fonts/Dangrek-Regular.ttf",
@@ -45,6 +47,34 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+                  <Head>
+        <Script
+  id="facebook-pixel"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '1054693372933429');
+      fbq('track', 'PageView');
+    `,
+  }}
+/>
+<noscript>
+  <img
+    height="1"
+    width="1"
+    style={{ display: "none" }}
+    src="https://www.facebook.com/tr?id=1054693372933429&ev=PageView&noscript=1"
+  />
+</noscript>
+      </Head>
       <body
         className={`${Baskerville.className} bg-white w-screen overflow-x-hidden antialiased`}
       >
--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,7 @@ import BestSellers from "./components/sections/BestSellers";
 import { ModalProvider } from '../app/context/ModalContext'
 import ProductModal from './components/ProductModal'
 import Head from "next/head";
+import Script from "next/script";
 
 export default function Home() {
   const OPTIONS: EmblaOptionsType = { loop: true }
@@ -24,7 +25,6 @@ const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
 
   return (
     <>
-          <Head>
         {/* Meta Pixel Script */}
         {/* <script
           dangerouslySetInnerHTML={{
@@ -39,37 +39,18 @@ const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
               'https://connect.facebook.net/en_US/fbevents.js');
               fbq('init', '1054693372933429');
               fbq('track', 'PageView');
-            `,
-          }}
-        />
-        <noscript>
+              `,
+              }}
+              />
+              <noscript>
           <img
             height="1"
             width="1"
             style={{ display: 'none' }}
             src="https://www.facebook.com/tr?id=1054693372933429&ev=PageView&noscript=1"
-          />
-        </noscript> */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1054693372933429');
-fbq('track', 'PageView');
-            `,
-          }}
-        />
-<noscript><img height="1" width="1" style={{display:'none'}}
-src="https://www.facebook.com/tr?id=1054693372933429&ev=PageView&noscript=1"
-/></noscript>
-      </Head>
+            />
+            </noscript> */}
+
     <div className="flex min-w-full overflow-x-hidden w-full bg-transparent flex-col items-center pt-14 justify-center h-auto min-h-screen ">
       {/* <Hero/> */}
       {/* <PrismaCarousel/> */}
